463cc7d315b2eeb15af8420c89b09fb3ddeabc08
Cristhian <79179637+Expectatives@users.noreply.github.com>
2021-06-21 13:39:44 -0500

    Create CC.java
    All the code was reworked and more packages were added to make it look neater.

---
+++ src/dev/expectatives/viperelevators/util/CC.java
@@ -0,0 +1,19 @@
+package dev.expectatives.viperelevators.util;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+
+/**
+ * @author Cristhian (Expectatives)
+ * lunes, junio 21, 2021
+ */
+
+public class CC {
+	
+	public static String translate(String i) {
+        return ChatColor.translateAlternateColorCodes('&', i);
+    }
+    public static void log(String i) {
+        Bukkit.getConsoleSender().sendMessage(translate(i));
+    }
+}
